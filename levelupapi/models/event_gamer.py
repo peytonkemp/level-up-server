9439d58b0b13b2f2b7b27124e6914c4c86fe4dea
peytonkemp <peyton.kemp@gmail.com>
2021-08-04 09:25:49 -0500

    first commit

---
+++ levelupapi/models/event_gamer.py
@@ -0,0 +1,9 @@
+
+from django.db import models
+
+
+class EventGamer(models.Model):
+    """Join model for Events and Gamers
+    """
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE)
+    event = models.ForeignKey("Event", on_delete=models.CASCADE)
---
+++ levelupapi/views/__init__.py
@@ -0,0 +1,3 @@
+from .auth import login_user, register_user
+from .gametype import GameTypeView, GameTypeSerializer, GameType
+from .game import Game, GameView, GameSerializer
